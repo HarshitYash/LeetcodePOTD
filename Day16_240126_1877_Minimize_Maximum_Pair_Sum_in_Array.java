b70eb877446ba0f8a2cb8624e53b03b8ca2825fa
Harshit Sahu <harshitsahu616@gmail.com>
2026-03-24 20:21:02 +0530

    24-01-2026

--- Day16_240126_1877_Minimize_Maximum_Pair_Sum_in_Array.java
+++ Day16_240126_1877_Minimize_Maximum_Pair_Sum_in_Array.java
@@ -1,5 +1,5 @@
 public class Day16_240126_1877_Minimize_Maximum_Pair_Sum_in_Array {
-    public int minPairSum(int[] nums) {
+    public int minPairSum(int[] nums){
         Arrays.sort(nums);
         int i=0, j=nums.length-1;
         int answer = 0;
