4f55a03aa13aa7b3500cf1cce23f7efd4fc249e6
Harshit Sahu <harshitsahu616@gmail.com>
2026-03-24 20:20:29 +0530

    11-03-2026

--- Day62_110326_1009_Complement_of_Base_10_Integer.java
+++ Day62_110326_1009_Complement_of_Base_10_Integer.java
@@ -1,5 +1,5 @@
 public class Day62_110326_1009_Complement_of_Base_10_Integer {
-    public int bitwiseComplement(int n) {
+    public int bitwiseComplement (int n) {
         if(n == 0) return 1;
         long temp = n;
         long mask = 0;
