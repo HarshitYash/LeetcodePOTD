9f5765462119f1e63eb24296058c70bcba892b00
Harshit Sahu <harshitsahu616@gmail.com>
2026-03-27 22:09:17 +0530

    27-03-2026

---
+++ Day78_270326_2946_Matrix_Similarity_After_Cyclic_Shifts.java
@@ -0,0 +1,13 @@
+public class Day78_270326_2946_Matrix_Similarity_After_Cyclic_Shifts {
+    public boolean areSimilar(int[][] mat, int k) {
+        int m = mat.length, n = mat[0].length;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(mat[i][j] != mat[i][(j+k)%n]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
